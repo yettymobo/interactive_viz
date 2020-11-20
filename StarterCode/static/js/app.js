2a3bfdf03f62c2c746f1470e4b0e3ee3eea4ee54
Patricia Mobolade <pmob@live.com>
2020-11-20 00:47:16 -0600

    Troubleshooting and main code complete

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,119 +1,103 @@
-// function buildData(sample) {
-    d3.json("../data/samples.json").then(function(data) {
+function init() {
+  // Grab a reference to the dropdown select element
+  var selection = d3.select("#selDataset");
 
-      console.log(data)})};
+  d3.json("/samples.json").then((data) => {
+    var sampleNames = data[0].names;
+   
+    sampleNames.forEach((sample) => {
+      selection
+        .append("option")
+        .text(sample)
+        .property("value", sample)
+    })
+    buildCharts(sampleNames[0]);
+  })
+  
+};
 
+function optionChanged(newSample) {
+  // Fetch new data each time a new sample is selected
+  buildCharts(newSample);
+  
+}
 
-// function buildData(sample) {
-//   d3.json("../data/samples.json").then(function(data) {
+// Initialize 
+init();
+
+//function when sample id is changed 
+function buildCharts(sampleid) {
+  //update demographic info area
+  var demoarea = d3.select('#sample-metadata');
+  d3.json("samples.json").then((data) => {
     
-//     var metadata = data.metadata;
-   
-//      // filter results for each ID
-//     var IDs = metadata.filter(row => row.id == sample);
-//     firstID = IDs[0]
-  
-//      // select panel object
-//     var panel = d3.select('#sample-metadata');
-//     panel.html('');
-  
-//     Object.entries(firstID).forEach(([key, value]) => {
-//         console.log([key, value])
-//         panel.append('p').text(`${key}: ${value}`)
-//     });
-//   })
-//   };
-
-//   function buildCharts(sample) {
-//     d3.json("../data/samples.json").then((data) => {
-//       var samples= data.samples;
-//       var findings= samples.filter(d => d.id == sample);
-//       var finding = findings[0]
-  
-//       var ids = finding.otu_ids;
-//       var labels = finding.otu_labels;
-//       var values = finding.sample_values;
-  
-//        //  Build bar Chart
+    var metadata = data[0].metadata;
+       
+    // filter results for each ID
+      var IDs = metadata.filter(row => row.id == sampleid)
+      console.log(IDs);
+      var firstID = IDs[0];
       
-//        var bar_data =[
-//         {
-//           y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
-//           x:values.slice(0,10).reverse(),
-//           text:labels.slice(0,10).reverse(),
-//           type:"bar",
-//           orientation:"h"
-  
-//         }
-//       ];
-  
-//       var barLayout = {
-//         title: "Top 10 Bacteria Cultures",
-//         margin: { t: 30, l: 150 }
-//       };
-  
-//       Plotly.newPlot("bar", bar_data, barLayout);
+      //clear list
+      demoarea.html("")
+      Object.entries(firstID).forEach(([key, value]) => {
+          demoarea.append("p").text(`${key}: ${value}`)
+      })
+            
+      var findings= data[0].samples.filter(d => d.id == sampleid);
+      var finding = findings[0];
 
+      var ids = finding.otu_ids;
+      var labels = finding.otu_labels;
+      var values = finding.sample_values;
+  
+       //  Build bar Chart
+      
+       var trace =[
+        {
+          y:ids.slice(0, 10).map(otuID => `OTU ${otuID}`).reverse(),
+          x:values.slice(0,10).reverse(),
+          text:labels.slice(0,10).reverse(),
+          type:"bar",
+          marker: {
+            color: 'rgb(142,124,195)'
+          },
+          orientation:"h"
+  
+        }
+      ];
+  
+      var layout = {
+        title: "Top 10 Bacteria Cultures per Individual",
+        
+      };
+  
+      Plotly.newPlot("bar", trace, layout);
 
-//       // Build a Bubble Chart using the sample data
-//       var BubbleChart = {
-//         margin: { t: 0 },
-//         xaxis: { title: "Sample IDs" },
-//         hovermode: "closest",
-//         };
-  
-//         var Bubble = [
-//         {
-//           x: ids,
-//           y: values,
-//           text: labels,
-//           mode: "markers",
-//           marker: {
-//             color: ids,
-//             size: values,
-//             }
-//         }
-//       ];
-  
-//       Plotly.plot("bubble", Bubble, BubbleChart);
-  
-     
-//     });
-//   }
-     
-  
-  
-//   function init() {
-//     // Grab a reference to the dropdown select element
-//     var selector = d3.select("#selDataset");
-  
-   
-  
-//     d3.json("samples.json").then((data) => {
-//       var sampleNames = data.names;
-//       //for (var i=0; i<sampleNames.length; i++){
-//       //  select
-//       //}
-//       sampleNames.forEach((sample) => {
-//         selector
-//           .append("option")
-//           .text(sample)
-//           .property("value", sample);
-//       });
-  
-//       // Use the first sample from the list to build the initial plots
-//       const firstSample = sampleNames[0];
-//       buildCharts(firstSample);
-//       buildMetadata(firstSample);
-//     });
-//     // console.log(json_obj)
-//   }
-  
-//   function optionChanged(newSample) {
-//     // Fetch new data each time a new sample is selected
-//     buildCharts(newSample);
-//     buildMetadata(newSample);
-//   }
-  
-//   // Initialize the dashboard
-//   init();
+      
+
+      //create bubble chart
+      var trace2 = {
+        x: finding.otu_ids,
+        y: finding.sample_values,
+        text: finding.otu_labels,
+        mode: "markers",
+        marker: {
+          color: finding.otu_ids,
+          size: finding.sample_values
+          }
+      };
+
+      var data = [trace2];
+
+      var layout = {
+        
+        showlegend: false,
+        margin: { t: 0 },
+        xaxis: { title: "OTU ID" },
+        hovermode: "closest",
+        };
+
+      Plotly.newPlot('bubble', data, layout);
+  });
+};
